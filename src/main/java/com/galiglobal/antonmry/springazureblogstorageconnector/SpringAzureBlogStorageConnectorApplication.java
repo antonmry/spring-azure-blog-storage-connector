7b57c43e490d2ea0a8174cc8a15c2acfe940729f
Antón R. Yuste <antonmry@inditex.com>
2020-01-28 10:18:05 +0000

    Initial commit

---
+++ src/main/java/com/galiglobal/antonmry/springazureblogstorageconnector/SpringAzureBlogStorageConnectorApplication.java
@@ -0,0 +1,13 @@
+package com.galiglobal.antonmry.springazureblogstorageconnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAzureBlogStorageConnectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAzureBlogStorageConnectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/galiglobal/antonmry/springazureblogstorageconnector/SpringAzureBlogStorageConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galiglobal.antonmry.springazureblogstorageconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAzureBlogStorageConnectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
