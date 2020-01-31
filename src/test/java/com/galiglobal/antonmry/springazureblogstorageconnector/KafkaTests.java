8b8657075f60b4e3dfbcf904762429c3c41900a2
Antón R. Yuste <antonmry@inditex.com>
2020-01-31 10:42:19 +0000

    Added Azure tests

--- src/test/java/com/galiglobal/antonmry/springazureblogstorageconnector/KafkaTests.java
+++ src/test/java/com/galiglobal/antonmry/springazureblogstorageconnector/KafkaTests.java
@@ -1,5 +1,10 @@
 package com.galiglobal.antonmry.springazureblogstorageconnector;
 
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
+import com.azure.storage.blob.specialized.BlockBlobClient;
+import com.azure.storage.common.StorageSharedKeyCredential;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
@@ -11,6 +16,7 @@ import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -19,7 +25,9 @@ import org.springframework.kafka.test.rule.EmbeddedKafkaRule;
 import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.*;
 import java.util.Collections;
+import java.util.Locale;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -42,7 +50,7 @@ public class KafkaTests {
     }
 
     @Test
-    public void testSendReceive() {
+    public void testSendReceive() throws IOException {
         Map<String, Object> senderProps = KafkaTestUtils.producerProps(embeddedKafka.getEmbeddedKafka());
         senderProps.put("key.serializer", ByteArraySerializer.class);
         senderProps.put("value.serializer", ByteArraySerializer.class);
@@ -65,8 +73,52 @@ public class KafkaTests {
 
         log.info("spring.cloud.stream.kafka.binder.brokers", embeddedKafka.getEmbeddedKafka().getBrokersAsString());
         assertThat(records.count()).isEqualTo(1);
+
+        uploadToAzureStorageBlob(records.iterator().next().value());
+
         assertThat(new String(records.iterator().next().value())).isEqualTo("FOO");
     }
+
+    // Azure test
+
+    @Value("${azure.storage.account-name}")
+    String accountName;
+
+    @Value("${azure.storage.account-key}")
+    String accountKey;
+
+    @Value("${azure.storage.container-name}")
+    String containerName;
+
+    public void uploadToAzureStorageBlob(byte[] data) throws IOException {
+
+        StorageSharedKeyCredential credential = new StorageSharedKeyCredential(accountName, accountKey);
+
+        String endpoint = String.format(Locale.ROOT, "https://%s.blob.core.windows.net", accountName);
+
+        BlobServiceClient storageClient = new BlobServiceClientBuilder()
+                .endpoint(endpoint)
+                .credential(credential)
+                .buildClient();
+
+        BlobContainerClient blobContainerClient = storageClient
+                .getBlobContainerClient(containerName);
+
+        BlockBlobClient blobClient = blobContainerClient.getBlobClient("HelloWorld.txt").getBlockBlobClient();
+
+        InputStream dataStream = new ByteArrayInputStream(data);
+
+        blobClient.upload(dataStream, data.length, true);
+
+        // Download the blob's content to output stream for debugging
+        OutputStream outStream = new ByteArrayOutputStream();
+        blobClient.download(outStream);
+        assertThat(outStream.toString()).isEqualTo("FOO");
+        outStream.close();
+
+        dataStream.close();
+    }
+
 
 }
 
