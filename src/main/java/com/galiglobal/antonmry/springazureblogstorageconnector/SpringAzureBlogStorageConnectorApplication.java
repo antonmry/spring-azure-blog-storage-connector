3d685e06e5e410423722de417d65ec31d840cdde
Antón R. Yuste <antonmry@inditex.com>
2020-01-31 10:17:57 +0000

    Migrated to Maven and Spring Cloud Stream kafka

--- src/main/java/com/galiglobal/antonmry/springazureblogstorageconnector/SpringAzureBlogStorageConnectorApplication.java
+++ src/main/java/com/galiglobal/antonmry/springazureblogstorageconnector/SpringAzureBlogStorageConnectorApplication.java
@@ -3,11 +3,19 @@ package com.galiglobal.antonmry.springazureblogstorageconnector;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Function;
 
 @SpringBootApplication
 @Slf4j
 public class SpringAzureBlogStorageConnectorApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringAzureBlogStorageConnectorApplication.class);
+    }
+
+    @Bean
+    public Function<byte[], byte[]> handle() {
+        return in -> new String(in).toUpperCase().getBytes();
     }
 }
--- src/test/java/com/galiglobal/antonmry/springazureblogstorageconnector/KafkaTests.java
+++ src/test/java/com/galiglobal/antonmry/springazureblogstorageconnector/KafkaTests.java
@@ -1,141 +1,72 @@
 package com.galiglobal.antonmry.springazureblogstorageconnector;
 
-import com.azure.storage.blob.BlobContainerClient;
-import com.azure.storage.blob.BlobServiceClient;
-import com.azure.storage.blob.BlobServiceClientBuilder;
-import com.azure.storage.blob.specialized.BlockBlobClient;
-import com.azure.storage.common.StorageSharedKeyCredential;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.common.serialization.ByteArrayDeserializer;
+import org.apache.kafka.common.serialization.ByteArraySerializer;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
-import org.springframework.kafka.listener.SeekToCurrentErrorHandler;
-import org.springframework.kafka.support.converter.RecordMessageConverter;
-import org.springframework.kafka.support.converter.StringJsonMessageConverter;
-import org.springframework.kafka.test.context.EmbeddedKafka;
-import org.springframework.util.backoff.FixedBackOff;
+import org.springframework.kafka.test.rule.EmbeddedKafkaRule;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.util.Locale;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
+import java.util.Collections;
+import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@EmbeddedKafka(topics = {"topic1", "topic1.DLT"},
-        bootstrapServersProperty = "spring.kafka.bootstrap-servers",
-        controlledShutdown = true)
+@RunWith(SpringRunner.class)
 @SpringBootTest
 @Slf4j
 public class KafkaTests {
 
-    @Autowired
-    private KafkaTemplate<Object, Object> template;
+    private static final String INPUT_TOPIC = "testEmbeddedIn";
+    private static final String OUTPUT_TOPIC = "testEmbeddedOut";
+    private static final String GROUP_NAME = "embeddedKafkaApplication";
 
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(
-            ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
-            ConsumerFactory<Object, Object> kafkaConsumerFactory,
-            KafkaTemplate<Object, Object> template) {
-        ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        configurer.configure(factory, kafkaConsumerFactory);
-        factory.setErrorHandler(new SeekToCurrentErrorHandler(
-                // dead-letter after 3 tries
-                new DeadLetterPublishingRecoverer(template), new FixedBackOff(0L, 2)));
-        factory.setMessageConverter(new StringJsonMessageConverter());
-        return factory;
+    @ClassRule
+    public static EmbeddedKafkaRule embeddedKafka = new EmbeddedKafkaRule(1, true, OUTPUT_TOPIC);
+
+    @BeforeClass
+    public static void setup() {
+        System.setProperty("spring.cloud.stream.kafka.binder.brokers", embeddedKafka.getEmbeddedKafka().getBrokersAsString());
     }
-
-    @Bean
-    public RecordMessageConverter converter() {
-        return new StringJsonMessageConverter();
-    }
-
-    final CountDownLatch latch = new CountDownLatch(4);
-
-    @KafkaListener(id = "fooGroup", topics = "topic1", containerFactory = "kafkaListenerContainerFactory")
-    public void listen(Foo2 foo) throws IOException {
-        log.info("Received: " + foo);
-        uploadToAzureStorageBlob(foo.toString());
-        latch.countDown();
-    }
-
-    @KafkaListener(id = "dltGroup", topics = "topic1.DLT")
-    public void dltListen(String in) {
-        log.info("Received from DLT: " + in);
-    }
-
 
     @Test
-    public void testKafka() throws Exception {
+    public void testSendReceive() {
+        Map<String, Object> senderProps = KafkaTestUtils.producerProps(embeddedKafka.getEmbeddedKafka());
+        senderProps.put("key.serializer", ByteArraySerializer.class);
+        senderProps.put("value.serializer", ByteArraySerializer.class);
+        DefaultKafkaProducerFactory<byte[], byte[]> pf = new DefaultKafkaProducerFactory<>(senderProps);
+        KafkaTemplate<byte[], byte[]> template = new KafkaTemplate<>(pf, true);
+        template.setDefaultTopic(INPUT_TOPIC);
+        template.sendDefault("foo".getBytes());
 
-        // Send
-        template.send("topic1", new Foo2("foo"));
-        template.send("topic1", new Foo2("bar"));
-        template.send("topic1", new Foo2("baz"));
-        template.send("topic1", new Foo2("qux"));
-        template.flush();
-        assertTrue(latch.await(60, TimeUnit.SECONDS));
-    }
+        Map<String, Object> consumerProps = KafkaTestUtils.consumerProps(GROUP_NAME, "false", embeddedKafka.getEmbeddedKafka());
+        consumerProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        consumerProps.put("key.deserializer", ByteArrayDeserializer.class);
+        consumerProps.put("value.deserializer", ByteArrayDeserializer.class);
+        DefaultKafkaConsumerFactory<byte[], byte[]> cf = new DefaultKafkaConsumerFactory<>(consumerProps);
 
-    @Value("${azure.storage.account-name}")
-    String accountName;
+        Consumer<byte[], byte[]> consumer = cf.createConsumer();
+        consumer.subscribe(Collections.singleton(OUTPUT_TOPIC));
+        ConsumerRecords<byte[], byte[]> records = consumer.poll(10_000);
+        consumer.commitSync();
 
-    @Value("${azure.storage.account-key}")
-    String accountKey;
 
-    @Value("${azure.storage.container-name}")
-    String containerName;
-
-    public void uploadToAzureStorageBlob(String data) throws IOException {
-
-        StorageSharedKeyCredential credential = new StorageSharedKeyCredential(accountName, accountKey);
-
-        String endpoint = String.format(Locale.ROOT, "https://%s.blob.core.windows.net", accountName);
-
-        BlobServiceClient storageClient = new BlobServiceClientBuilder()
-                .endpoint(endpoint)
-                .credential(credential)
-                .buildClient();
-
-        BlobContainerClient blobContainerClient = storageClient
-                .getBlobContainerClient(containerName);
-
-        BlockBlobClient blobClient = blobContainerClient.getBlobClient("HelloWorld.txt").getBlockBlobClient();
-
-        InputStream dataStream = new ByteArrayInputStream(data.getBytes(StandardCharsets.UTF_8));
-
-        blobClient.upload(dataStream, data.length(), true);
-
-        dataStream.close();
-
-        // Download the blob's content to output stream.
-        int dataSize = (int) blobClient.getProperties().getBlobSize();
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(dataSize);
-        blobClient.download(outputStream);
-        outputStream.close();
-
-        assertEquals(data, new String(outputStream.toByteArray(), StandardCharsets.UTF_8));
-
-        // Delete the blob we created earlier.
-        // blobClient.delete();
-
+        log.info("spring.cloud.stream.kafka.binder.brokers", embeddedKafka.getEmbeddedKafka().getBrokersAsString());
+        assertThat(records.count()).isEqualTo(1);
+        assertThat(new String(records.iterator().next().value())).isEqualTo("FOO");
     }
 
 }
-
-
 
